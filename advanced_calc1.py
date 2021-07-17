599d3be81c0d8acd4fa00ab1877c53f035a4d069
Rekluse <87527570+InternetRekluse@users.noreply.github.com>
2021-07-17 00:13:39 -0500

    Add files via upload

---
+++ advanced_calc1.py
@@ -0,0 +1,18 @@
+# code made by: internetrekluse.
+
+# enter numbers only, if else:
+# there will be errors.
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+  print(num1 + num2)
+elif op == "-":
+  print(num1 - num2)
+elif op == "/":
+  print(num1 / num2)
+elif op == "*":
+  print(num1 * num2)
+else:
+  print("Invalid operator given!")
